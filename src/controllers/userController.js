102dad9eaf2ced837f91ecbb08072be683cb20b0
HasibGit <hasibullah04@gmail.com>
2023-11-11 03:57:15 +0600

    user update endpoint done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -106,8 +106,45 @@ const getUserById = async (req, res) => {
   }
 };
 
+const updateUser = async (req, res) => {
+  try {
+    if (req.params.id !== req.userId) {
+      res.status(400).json({ message: "Bad request" });
+    }
+
+    const updatedUserData = req.body;
+
+    const updatedUser = await userModel.findOneAndUpdate(
+      { _id: req.userId },
+      { $set: updatedUserData },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const expiresIn = 3600; // in seconds
+    const token = jwt.sign(
+      { id: updatedUser._id, email: updatedUser.Email },
+      SECRET_KEY,
+      { expiresIn }
+    );
+
+    res.status(200).json({
+      token: token,
+      userId: updatedUser._id,
+      email: updatedUser.Email,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+};
+
 module.exports = {
   signup,
   login,
   getUserById,
+  updateUser,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { signup, login, getUserById } = require("../controllers/userController");
+const {
+  signup,
+  login,
+  getUserById,
+  updateUser,
+} = require("../controllers/userController");
 const auth = require("../middlewares/auth");
 const userRouter = express.Router();
 
@@ -7,6 +12,8 @@ userRouter.post("/signup", signup);
 
 userRouter.post("/login", login);
 
-userRouter.get("/getUserById/:id", auth, getUserById);
+userRouter.get("/get-user-by-Id/:id", auth, getUserById);
+
+userRouter.post("/update-user/:id", auth, updateUser);
 
 module.exports = userRouter;
